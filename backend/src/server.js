9fadd79d36cf058ce07b2dd2154daadee4a0317f
TaiZuon <124855937+TaiZuon@users.noreply.github.com>
2024-10-19 22:47:41 +0700

    init: re-structure project folder

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-
+const cors = require("cors");
 const express = require("express");
 const path = require("path");
 const app = express();
@@ -7,23 +7,15 @@ const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
 const configViewEngine = require("./config/viewengine");
 const webRoutes = require("./routes/web");
-const connection = require("./config/databaseConnection");
-
 //config template engine
 configViewEngine(app);
 
-//routes init
-app.use("/", webRoutes);
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-//test the database connection
-connection.query(
-  "SELECT * FROM chizzakura.items;",
-  function (err, rows, fields) {
-    if (err) throw err;
-    console.log("The solution is: ", rows);
-    console.log("The fields is: ", fields);
-  }
-);
+//User Management routes
+app.use("/UM/", webRoutes.router);
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}!`);
