46beba62295f02b1089f91301e8356eeb16adeec
cusnaruto <hoangvip3333@gmail.com>
2024-11-19 00:42:17 +0700

    action: added crud for discounts, and split the routes correctly

---
+++ backend/src/routes/userRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const { createUser, findUserByUsername } = require("../controllers/userController");
+
+router.post("/create-customer", createUser);
+router.get("/sample", findUserByUsername);
+router.get("/hello", (req, res) => {
+  res.json({ message: "Hello from the backend!" });
+});
+
+module.exports = router;
