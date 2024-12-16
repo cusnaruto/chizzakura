42035366855d93ab2d03892ac14af4a199e8ff7c
TaiZuon <124855937+TaiZuon@users.noreply.github.com>
2024-12-16 23:28:45 +0700

    fix: database config for platform

---
+++ backend/src/config/config.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || "root",
+    database: process.env.DB_NAME || "chizzakura",
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: process.env.DB_PORT || "3308",
+    dialect: "mysql",
+  },
+  test: {
+    username: process.env.TEST_DB_USERNAME || $YOUR_ACCOUNT,
+    password: process.env.TEST_DB_PASSWORD || "chizzakura",
+    database: process.env.TEST_DB_NAME || `${$YOUR_ACCOUNT}_chizzakura`,
+    host: process.env.TEST_DB_HOST || $MYSQL_SERVICE_HOST,
+    port: process.env.TEST_DB_PORT || $MYSQL_SERVICE_PORT,
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.PROD_DB_USERNAME || "root",
+    password: process.env.PROD_DB_PASSWORD || null,
+    database: process.env.PROD_DB_NAME || "database_production",
+    host: process.env.PROD_DB_HOST || "127.0.0.1",
+    dialect: "mysql",
+  },
+};
--- backend/src/config/databaseConnection.js
+++ backend/src/config/databaseConnection.js
@@ -2,12 +2,12 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_NAME || `${$YOUR_ACCOUNT}_chizzakura`,
+  process.env.DB_USER || $YOUR_ACCOUNT,
+  process.env.DB_PASSWORD || "chizzakura",
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    host: process.env.DB_HOST || $MYSQL_SERVICE_HOST,
+    port: process.env.DB_PORT || $MYSQL_SERVICE_PORT,
     dialect: "mysql",
     timezone: "+07:00",
   }
